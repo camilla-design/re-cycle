479b8df3b3b3276ae847243b70543b56569a1857
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-05-26 22:04:15 +0200

    homepage almost done

---
+++ recycle/script.js
@@ -0,0 +1,9 @@
+
+function navigationBar() {
+    var navMenu = document.getElementById("nav-links");
+    if (navMenu.style.display === "block") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "block";
+    }
+}
